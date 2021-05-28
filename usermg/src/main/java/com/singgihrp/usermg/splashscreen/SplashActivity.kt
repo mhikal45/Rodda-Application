1bc35b3416d237abab268335ae4003081862faa5
srpradana <danapradana11@gmail.com>
2021-05-29 02:23:28 +0700

    Added click function

--- usermg/src/main/java/com/singgihrp/usermg/splashscreen/SplashActivity.kt
+++ usermg/src/main/java/com/singgihrp/usermg/splashscreen/SplashActivity.kt
@@ -3,13 +3,13 @@ package com.singgihrp.usermg.splashscreen
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.singgihrp.usermg.MainActivity
+import com.singgihrp.usermg.um.login.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(applicationContext, MainActivity::class.java))
+        startActivity(Intent(this, LoginActivity::class.java))
         finish()
     }
 }
--- usermg/src/main/java/com/singgihrp/usermg/um/register/RegistrerFormActivity.kt
+++ usermg/src/main/java/com/singgihrp/usermg/um/register/RegistrerFormActivity.kt
@@ -1,12 +1,23 @@
 package com.singgihrp.usermg.um.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.singgihrp.usermg.R
+import com.singgihrp.usermg.databinding.ActivityRegistrerFormBinding
+import com.singgihrp.usermg.um.login.LoginActivity
 
 class RegistrerFormActivity : AppCompatActivity() {
+
+    private lateinit var registerBinding: ActivityRegistrerFormBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrer_form)
+        registerBinding = ActivityRegistrerFormBinding.inflate(layoutInflater)
+
+        registerBinding.tvLogin.setOnClickListener{
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
