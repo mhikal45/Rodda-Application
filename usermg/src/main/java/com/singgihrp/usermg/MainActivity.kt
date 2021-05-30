4510c26d3597c9722685b65ed89111b800d6c661
srpradana <danapradana11@gmail.com>
2021-05-30 23:03:31 +0700

    added login and logut firebase auth

--- usermg/src/main/java/com/singgihrp/usermg/MainActivity.kt
+++ usermg/src/main/java/com/singgihrp/usermg/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.singgihrp.usermg
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.singgihrp.usermg.databinding.ActivityMainBinding
+import com.singgihrp.usermg.um.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogot.setOnClickListener{
+            FirebaseAuth.getInstance().signOut()
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
