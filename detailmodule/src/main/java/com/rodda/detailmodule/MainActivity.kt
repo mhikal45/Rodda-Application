8338179765650ba85532c76df47d652e5c350698
mhikal45 <a2292218@bangkit.academy>
2021-05-30 14:54:54 +0700

    adding detail activity

--- detailmodule/src/main/java/com/rodda/detailmodule/MainActivity.kt
+++ detailmodule/src/main/java/com/rodda/detailmodule/MainActivity.kt
@@ -2,10 +2,17 @@ package com.rodda.detailmodule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rodda.detailmodule.ui.dataform.DataFormFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, DataFormFragment.newInstance())
+                .commitNow()
+        }
     }
 }
