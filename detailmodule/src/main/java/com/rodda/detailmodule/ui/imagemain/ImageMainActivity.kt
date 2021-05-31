e2ae5782860e95d1d23b6f98114729e8d637efdc
mhikal45 <a2292218@bangkit.academy>
2021-05-31 08:36:50 +0700

    adding Image Main activity

--- detailmodule/src/main/java/com/rodda/detailmodule/ui/imagemain/ImageMainActivity.kt
+++ detailmodule/src/main/java/com/rodda/detailmodule/ui/imagemain/ImageMainActivity.kt
@@ -1,17 +1,17 @@
-package com.rodda.detailmodule
+package com.rodda.detailmodule.ui.imagemain
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.rodda.detailmodule.ui.dataform.DataFormFragment
+import com.rodda.detailmodule.R
 
-class MainActivity : AppCompatActivity() {
+class ImageMainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        setContentView(R.layout.image_main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, DataFormFragment.newInstance())
+                .replace(R.id.container_image_main, ImageMainFragment.newInstance())
                 .commitNow()
         }
     }
